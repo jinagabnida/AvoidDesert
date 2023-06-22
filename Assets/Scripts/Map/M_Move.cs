bc727457e674bac4dec4ffe0f3af8106f885d32d
김영진 <81694402+jinagabnida@users.noreply.github.com>
2023-06-22 09:33:47 +0900

    map
    move map

---
+++ Assets/Scripts/Map/M_Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class M_Move : MonoBehaviour
+{
+    public Transform MapLine;
+    public Transform Player;
+    public float speed = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MapLine.position.x >= Player.position.x)
+        {
+            transform.position = new Vector3(0.0f, 0.0f, 0.0f); 
+        }
+        transform.position += transform.right * speed;
+
+    }
+}
